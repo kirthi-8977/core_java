e769321ff137966d9407f5cc9430509358f0436a
kirthi-8977 <kirthir577@gmail.com>
2024-08-05 22:16:44 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+class evenodd
+{
+public static void main(String[] args)
+{
+for(int i=1;i<=100;i++){
+int a=i;
+if(a%2==0){
+System.out.println(a+"even Number");
+}
+}
+}
+
+ }
---
+++ prime.java
@@ -0,0 +1,13 @@
+class prime
+{
+public static void main(String args[])
+{
+for(int i=1;i<=100;i++)
+{
+int a=i;
+if(a%2==3)
+{
+System.out.println(a+ "prime Number");
+}
+}
+}
